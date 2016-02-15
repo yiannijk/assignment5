5638a036d7ccb7f4395310b9d65f435470ac8e8d
Kontorousis <dimitre@ugl11.ccs.itd.umich.edu>
2016-02-14 20:09:10 -0500

    pushing files for submission on canvas

--- Assignment5Final/ViewController.swift
+++ Assignment5Final/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet weak var slidervalue: UISlider!
+    
+    @IBOutlet weak var multiplier: UILabel!
+    
+    @IBAction func slidermoved(sender: UISlider) {
+        print(slidervalue)
+        table.reloadData()
+    }
+    
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) ->Int {
+        return 12
+    }
+    func tableView(tableView: UITableView,cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+       
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        let timesTable = Int(slidervalue.value * 12)
+        
+    multiplier.text = String(timesTable)
+    
+    cell.textLabel?.text = String(timesTable * indexPath.row)
+        return cell
+        
+        
+}
 }
 
